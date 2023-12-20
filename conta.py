26aa557a3e1c8be95a7e835785d17e38f0c936e3
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-20 13:46:55 -0400

    I learned about methods, garbage collector and the use the None

--- conta.py
+++ conta.py
@@ -1,8 +1,19 @@
 class Conta:
     def __init__(self,numero, titular, saldo, limite): #função construtora/posso passar um parâmetro opcional também para esses parâmetros
         print(f'Construindo objeto...{self}')#Self é a referência que onde encontrar o objeto, vai ser caminho para ir para cada objeto e forma distinta
-        # Instanciando objetos
+        # Abributos
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+    #Criando métodos
+    def extrato(self):
+        print(f'Titular: {self.titular}')
+        print(f'Saldo: {self.saldo}')
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
