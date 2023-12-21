c034c406c277e87a11c8c2cf605b65c0f100d3ee
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-21 17:52:32 -0400

    I tested somethings

--- conta.py
+++ conta.py
@@ -2,18 +2,21 @@ class Conta:
     def __init__(self,numero, titular, saldo, limite): #função construtora/posso passar um parâmetro opcional também para esses parâmetros
         print(f'Construindo objeto...{self}')#Self é a referência que onde encontrar o objeto, vai ser caminho para ir para cada objeto e forma distinta
         # Abributos
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero #colocando esses dois underscore o atributo fica privado é equivalente ao private no java porém tem que alterar em todos os métodos pois agora faz parte do nome da variável
+        self.__titular = titular #poded chamar usando por exemplo conta._Conta__saldo (<objeto>._<classe>__<atributo>) e fica oculto na recomendação
+        self.__saldo = saldo
+        self.__limite = limite
     #Criando métodos
     def extrato(self):
-        print(f'Titular: {self.titular}')
-        print(f'Saldo: {self.saldo}')
+        print(f'Titular: {self.__titular}')
+        print(f'Saldo: {self.__saldo}')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
+    def transfere(self, valor, origem, destino):
+        origem.saca(valor)
+        destino.deposita(valor)
